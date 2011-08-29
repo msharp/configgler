066404af243184f96d5766f97a5d5b3302c39a52
max sharples <maxsharples@gmail.com>
2011-08-29 17:11:04 +1000

    added basic code

--- lib/configgy.rb
+++ lib/configgy.rb
@@ -1,5 +1,64 @@
-require "configgy/version"
+"""
+nicer api to get data from the yaml configuration files
 
-module Configgy
-  # Your code goes here...
+usage example =>
+    c = Configgy.new('config.yml','./')
+    puts c.smtp.host.value
+OR
+    puts c.get(:smtp,:host)
+TODO : handle existing object methods (type,id,class)
+"""
+
+require "configgy/version"
+require 'yaml'
+
+class Configgy
+    
+    attr_accessor :config
+
+    def initialize(file,*args)
+        @config = YAML.load(File.new(File.join(args.join(','),file)))
+    end
+
+    def method_missing(name,*args)
+        ConfigElement.new(@config,name) 
+    end
+
+    def get_by_array(args=[])
+        c = @config[args.shift.to_s] 
+        until args.length == 0 
+            c = c[args.shift.to_s]
+        end
+        c
+    end
+
+    def get(*args)
+        get_by_array(args)
+    end
+
+    class ConfigElement
+
+        attr_accessor :element
+
+        def initialize(config,element)
+            @element = config[element.to_s]
+            self
+        end
+
+        def method_missing(name)
+            ConfigElement.new(@element,name.to_s)
+        end
+
+        def value
+            @element
+        end
+
+    end
+
 end
+
+=begin
+c = Configgy.new('config.yml','./conf')
+puts c.smtp.test.type.value
+puts c.get(:smtp,:test,:type)
+=end
